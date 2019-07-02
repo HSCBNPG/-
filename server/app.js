dc5ebb7655518e8842038d543ed67e4b02b9aaf6
xyx <123@qq.com>
2019-07-02 21:10:53 +0800

    注册栏调试成功

--- server/app.js
+++ server/app.js
@@ -4,9 +4,19 @@ const express=require('express');
 var app=express();
 // 引入路由器模块
 var router=require('./routes/user.js');
-// 使用路由器
-app.use('/user',router);
+
+
 // 托管静态资源到html下
 app.use( express.static('../html') );
+// 引入body-parser中间件
+const bodyparser=require('body-parser');
+// 设置bodyparser中间件转换查询字符串的方法
+app.use( bodyparser.urlencoded({
+	extended:false
+}) );
+
+
+// 使用路由器，挂载路由器要放在中间件下边
+app.use('/user',router);
 // 设置监听端口
 app.listen(8080);
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,38 @@
 const express=require('express');
 // 创建路由器
 var router=express.Router();
-// 创建路由
-router.get('/data',function(req,res){
-	res.send('hahaha');
+// 引入连接池模块
+var pool=require('../../mysql/pool.js');
+// 创建注册路由
+router.post('/reg',function(req,res){
+	// 获取post请求的值
+	var obj=req.body
+	// 验证是否为空
+	if(!obj.uname){
+		res.send('请输入用户名');
+		return;
+	}
+	if(!obj.upwd){
+		res.send('请输入密码');
+		return;
+	}
+	// 执行SQL语句，如果用户名重复，提示,如果不重复插入数据
+	pool.query('SELECT * FROM user WHERE uname=?',[obj.uname],function(err,result){
+		if(err)throw err;
+		// result是语句执行结果返回值，一个数组
+		if(result.length>0){
+			res.send('用户名重复');
+		}else{
+			// 如果不重复插入数据并提示注册成功
+			pool.query('INSERT INTO user SET ?',[obj],function(err,result){
+				if(err)throw err;
+				// result是一个对象affectedRows是result下的一个属性
+				if(result.affectedRows>0){
+					res.send('注册成功');
+				}
+			})
+		}
+	});
 });
 // 创建首页路由
 router.get('/index',function(req,res){
